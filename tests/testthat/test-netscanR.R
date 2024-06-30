53c153e199931193fa4e34de893a57e715f94c83
stibu81 <slanz1137@gmail.com>
2024-06-30 16:14:25 +0200

    install arp-scan in github actions

--- tests/testthat/test-netscanR.R
+++ tests/testthat/test-netscanR.R
@@ -1,7 +1,7 @@
 test_that("test find_arp_scan", {
   expect_vector(find_arp_scan(), ptype = character(0), size = 1)
   skip_on_cran()
-  skip_on_ci()
+  skip_on_os("mac")
   expect_equal(find_arp_scan(), "/usr/sbin/arp-scan")
 })
 
@@ -9,7 +9,7 @@ test_that("test find_arp_scan", {
 test_that("test arp_scan_version()", {
   expect_s3_class(arp_scan_version(), "numeric_version")
   skip_on_cran()
-  skip_on_ci()
+  skip_on_os("mac")
   expect_true(arp_scan_version() >= as.numeric_version("1.9.7"))
 })
 
