c52fbba6e2e81292a943ce6e27db629c1eaf11ba
stibu81 <slanz1137@gmail.com>
2024-07-15 17:36:36 +0200

    add functions to read and write netscanr db

--- R/db.R
+++ R/db.R
@@ -93,4 +93,112 @@ create_netscanr_table <- function(con, file) {
 }
 
 
-#
+#' Write to netscanR SQLite Database
+#'
+#' Write the output of a network scan with [`run_arp_scan()`] to an netscanR
+#' SQLite database.
+#'
+#' @param arp_scan_result tibble with the results of [`run_arp_scan()`].
+#' @param file path to the SQLite database created with [`create_netscanr_db()`].
+#' @param timestamp timestamp to be written to the database for this scan.
+#' If omitted, the current time is used.
+#'
+#' @returns
+#' the data written to the database, invisibly.
+#'
+#' @export
+
+write_netscanr_db <- function(arp_scan_result,
+                              file,
+                              timestamp = Sys.time()) {
+
+  con <- connect_netscanr_db(file)
+  withr::defer(RSQLite::dbDisconnect(con))
+
+  check_netscanr_table(con, file)
+
+  arp_scan_result <- arp_scan_result %>%
+    dplyr::mutate(timestamp = timestamp, .before = 1)
+
+  RSQLite::dbWriteTable(con, "netscanr", arp_scan_result, append = TRUE)
+
+  invisible(arp_scan_result)
+
+}
+
+
+#' Read netscanR SQLite Database
+#'
+#' Read data from a netscanR SQLite database.
+#'
+#' @param file path to the SQLite database.
+#' @param start,end start and end timestamp for the data to be read.
+#' @param tz time zone to be used for the timestamps. If omitted, the default
+#' time zone of the system is used.
+#'
+#' @returns
+#' a tibble with the data read from the database. It has all the columns
+#' of the tibble created by [`run_arp_scan()`] plus an additional column
+#' `timestamp` with the time when the scan was performed.
+#'
+#' @export
+
+read_netscanr_db <- function(file, start = NULL, end = NULL, tz = "") {
+
+  con <- connect_netscanr_db(file)
+  withr::defer(RSQLite::dbDisconnect(con))
+
+  check_netscanr_table(con, file)
+
+  table <- dplyr::tbl(con, "netscanr")
+
+  # filter by date if requested
+  #browser()
+  if (!is.null(start)) {
+    if (!lubridate::is.POSIXt(start)) {
+      cli::cli_abort("start must be a POSIXt object.")
+    }
+    table <- table %>% dplyr::filter(.data$timestamp >= !!as.numeric(start))
+  }
+  if (!is.null(end)) {
+    if (!lubridate::is.POSIXt(end)) {
+      cli::cli_abort("end must be a POSIXt object.")
+    }
+    table <- table %>% dplyr::filter(.data$timestamp <= !!as.numeric(end))
+  }
+
+  # if timezone is not set, pick the system time zone
+  if (tz == "") tz <- Sys.timezone()
+
+  #  collect the data and convert data types
+  table <- table %>%
+    dplyr::collect() %>%
+    dplyr::mutate(
+      timestamp = as.POSIXct(.data$timestamp, origin = "1970-01-01", tz = tz),
+      expected_ip = as.logical(.data$expected_ip),
+      known_device = as.logical(.data$known_device)
+    )
+
+  table
+}
+
+
+# check that the table netscanr exists in the database
+
+check_netscanr_table <- function(con, file, error_call = rlang::caller_env()) {
+
+  if (!"netscanr" %in% RSQLite::dbListTables(con)) {
+    cli::cli_abort(
+      c("!" = "Table \"netscanr\" does not exist in database {file}.",
+        "i" = paste(
+          "Use {.run",
+          "[create_netscanr_db(\"{file}\")](netscanR::create_netscanr_db(\"{file}\"))}",
+          "to create the database."
+        )
+      ),
+      call = error_call
+    )
+  }
+
+  invisible(TRUE)
+}
--- tests/testthat/test-db.R
+++ tests/testthat/test-db.R
@@ -1,5 +1,9 @@
 library(withr)
 library(RSQLite)
+library(lubridate, warn.conflicts = FALSE)
+library(dplyr, warn.conflicts = FALSE)
+library(dbplyr, warn.conflicts = FALSE)
+
 
 test_that("test create_netscanr_db()", {
   with_tempfile("netscanr_db", fileext = ".sqlite", {
@@ -66,4 +70,90 @@ test_that("test create_netscanr_db() with errors", {
     create_netscanr_db("netscanr.txt"),
     "netscanr\\.txt has the wrong extension"
   )
+})
+
+
+test_that("test reading and writing database", {
+  with_tempfile("netscanr_db", fileext = ".sqlite", {
+    arp_scan_table <- get_arp_scan_ref()
+    create_netscanr_db(netscanr_db)
+    ts <- ymd_hms(c("2024-03-07 10:00:00", "2024-06-12 07:00:00"),
+                  tz = Sys.timezone())
+    # write database
+    expect_equal(
+      write_netscanr_db(arp_scan_table, netscanr_db, ts[1]),
+      arp_scan_table %>% mutate(timestamp = ts[1], .before = 1)
+    )
+    expect_equal(
+      write_netscanr_db(arp_scan_table, netscanr_db, ts[2]),
+      arp_scan_table %>% mutate(timestamp = ts[2], .before = 1)
+    )
+    # read database with standard time zone
+    expect_equal(
+      read_netscanr_db(netscanr_db),
+      bind_rows(
+        arp_scan_table %>% mutate(timestamp = ts[1], .before = 1),
+        arp_scan_table %>% mutate(timestamp = ts[2], .before = 1)
+      )
+    )
+    # read database with different time zone
+    ts_la <- with_tz(ts, "America/Los_Angeles")
+    expect_equal(
+      read_netscanr_db(netscanr_db, tz = "America/Los_Angeles"),
+      bind_rows(
+        arp_scan_table %>% mutate(timestamp = ts_la[1], .before = 1),
+        arp_scan_table %>% mutate(timestamp = ts_la[2], .before = 1)
+      )
+    )
+    # read data with time filter
+    expect_equal(
+      read_netscanr_db(netscanr_db, start = ts[2] - days(1)),
+      bind_rows(
+        arp_scan_table %>% mutate(timestamp = ts[2], .before = 1)
+      )
+    )
+    expect_equal(
+      read_netscanr_db(netscanr_db, end = ts[1] + days(1)),
+      bind_rows(
+        arp_scan_table %>% mutate(timestamp = ts[1], .before = 1)
+      )
+    )
+    expect_equal(
+      read_netscanr_db(netscanr_db, start = ts[1] - days(1), end = ts[2] + days(1)),
+      bind_rows(
+        arp_scan_table %>% mutate(timestamp = ts[1], .before = 1),
+        arp_scan_table %>% mutate(timestamp = ts[2], .before = 1)
+      )
+    )
+    expect_equal(
+      read_netscanr_db(netscanr_db, start = ts[1] + days(1), end = ts[2] - days(1)),
+      arp_scan_table %>%
+        mutate(timestamp = ts[1], .before = 1) %>%
+        slice(integer(0))
+    )
+  })
+})
+
+
+test_that("test reading and writing database", {
+  with_tempfile("netscanr_db", fileext = ".sqlite", {
+    create_netscanr_db(netscanr_db)
+    # bad data type for start/end time
+    expect_error(
+      read_netscanr_db(netscanr_db, start = "2024-03-07 10:00:00"),
+      "start must be a POSIXt object"
+    )
+    expect_error(
+      read_netscanr_db(netscanr_db, end = "2024-03-07 10:00:00"),
+      "end must be a POSIXt object"
+    )
+    with_db_connection(list(con = dbConnect(SQLite(), netscanr_db)), {
+      # bad table name
+      dbExecute(con, "DROP TABLE netscanr")
+      expect_error(
+        read_netscanr_db(netscanr_db),
+        "Table \"netscanr\" does not exist in database"
+      )
+    })
+  })
 })
